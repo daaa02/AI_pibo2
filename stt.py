a78c516beee71986adbdcd65c77794c66fac9a94
dah02 <92973975+dah02@users.noreply.github.com>
2022-02-28 02:19:25 +0900

    Update and rename transcribe_streaming_mic.py to stt.py

--- stt.py
+++ stt.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# transcribe_streaming_mic.py 
+
 """Google Cloud Speech API sample application using the streaming API.
 
 NOTE: This module requires the additional dependency `pyaudio`. To install
