8bac004a964123244975e15a3f958a2581ec2ffb
dah02 <92973975+dah02@users.noreply.github.com>
2022-02-28 17:50:18 +0900

    Update NLP.py

--- src/NLP.py
+++ src/NLP.py
@@ -19,6 +19,10 @@ class Dictionary:
         self.Again = ['again', '다시', '또', '같은', '한 번 더', '한번 더', '계속']
 
         self.Animal = ['치타', '타조', '돌고래', '사슴', '호랑이', '고양이', '강아지', '수달', '코끼리', '토끼', '사자', '표범']
+        
+        self.Number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        
+        self.Number_word = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구', '십']
 
 
 class NLP:
@@ -30,6 +34,7 @@ class NLP:
         self.temp = {}
         self.n = 0
 
+        
     def nlp_answer(self, user_said, dic):
         answer = ''
         for i in range(len(dic.Yes)):
@@ -46,6 +51,7 @@ class NLP:
                 answer = 'AGAIN'
         return answer
 
+    
     def nlp_animal(self, user_input, dic):
         """
         기존에는 '가, 랑, 을, 를, ( )' 을 split 했는데, 이러면 삭제되는 동물들이 있움 ex.호랑이
@@ -55,3 +61,21 @@ class NLP:
 
         answer = [i for i in a_list if i in dic.animal]
         return answer[0]
+    
+    
+    def nlp_number(self, user_said, dic):
+        answer = -1
+        ko = -1
+        nb = -1
+        for i, j in enumerate(dic.Number_word):
+            x = user_said.find(j)
+            if x != -1:
+                ko = i
+        for i, j in enumerate(dic.Number):
+            x = user_said.find(j)
+            if x != -1:
+                nb = i
+        answer = max(ko, nb)
+        return answer
+    
+    
