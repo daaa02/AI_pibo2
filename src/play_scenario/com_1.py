e2141095e42d8c5604e459c114c27fd80981412b
daaa02 <dyk98498@gmail.com>
2022-03-16 19:34:41 +0900

    Update com_1.py

--- src/play_scenario/com_1.py
+++ src/play_scenario/com_1.py
@@ -271,10 +271,10 @@ def Play_Hoop(user_name):
     behavior_list.do_suggestion_L()
     while True:
         text_to_speech("사진을 찍어 줄게. 가장 마음에 드는 동물 그림을 들고 브이해봐!")
-        tts.play(filename="/home/pi/AI_pibo2/src/data/audio/사진기소리.mp3", out='local', volume=-1000, background=False)
         break
 
     behavior_list.do_photo()
+    tts.play(filename="/home/pi/AI_pibo2/src/data/audio/사진기소리.mp3", out='local', volume=-1000, background=False)
 
     # 2.7 다음 놀이 제안
     behavior_list.do_question_L()
