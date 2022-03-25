201ad694cb10fd2205a9e353b2eb3ab6214a5ac3
daaa02 <dyk98498@gmail.com>
2022-03-25 17:10:29 +0900

    Update eye_list.py

--- src/data/eye_list.py
+++ src/data/eye_list.py
@@ -18,40 +18,40 @@ device = Device()
 
 
 def e_question():
-    device.send_cmd(21, '108,209,239,10')
+    device.send_cmd(20, '108,209,239')
 
 
 def e_suggestion():
-    device.send_cmd(21, '108,209,239,10')
+    device.send_cmd(21, '108,209,239,100')
 
 
 def e_explain():
-    device.send_cmd(21, '108,209,239,10')
+    device.send_cmd(21, '108,209,239,100')
 
 
 def e_photo():
-    device.send_cmd(20, '120,230,208')
+    device.send_cmd(20, '153,255,51')
 
 
 def e_stamp():
-    device.send_cmd(25, '20')
+    device.send_cmd(21, '255,51,255,500')
 
 
 def e_waiting():
-    device.send_cmd(21, '108,209,239,20')
+    device.send_cmd(21, '108,209,239,500')
 
 
 def e_praise():
-    device.send_cmd(25, '20')
+    device.send_cmd(25, '100')
 
 
 def e_agree():
-    device.send_cmd(21, '108,209,239,20')
+    device.send_cmd(20, '108,209,239')
 
 
 def e_joy():
-    device.send_cmd(25, '10')
+    device.send_cmd(25, '100')
 
 
 def e_sad():
-    device.send_cmd(21, '186,147,223,50')
+    device.send_cmd(21, '186,147,223,500')
