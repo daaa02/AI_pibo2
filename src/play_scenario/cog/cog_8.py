1040c9b757251fe234d6aac11b54ddb4fc9d0243
daaa02 <dyk98498@gmail.com>
2022-08-03 16:47:49 +0900

    test

--- src/play_scenario/cog/cog_8.py
+++ src/play_scenario/cog/cog_8.py
@@ -48,7 +48,7 @@ def Play_Sound(user_name):
 
     behavior_list.do_waiting_A()
     while True:
-        user_said = input("답변 : ")    # stt open
+        user_said = speech_to_text()    # stt open
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)   # stt 결과 처리 (NLP.py 참고)
 
         if answer == 'DONE':
@@ -74,7 +74,7 @@ def Play_Sound(user_name):
         time.sleep(1)
         text_to_speech("할 수 있지? 할 수 있으면 할 수 있어라고 말해줘~")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
         if answer == 'YES':
@@ -92,7 +92,7 @@ def Play_Sound(user_name):
     while True:
         text_to_speech("준비가 됐으면 시작하자고 말해줘~")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
         if answer == 'DONE':
@@ -119,7 +119,7 @@ def Play_Sound(user_name):
         while True:
             text_to_speech("준비가 됐으면 준비됐어 라고 말해줘~")
 
-            user_said = input("답변 : ")
+            user_said = speech_to_text()
             answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
             if answer == 'DONE':
@@ -138,7 +138,7 @@ def Play_Sound(user_name):
         behavior_list.do_question_S()
         while True:
             text_to_speech("어떤 소리가 나? 정답을 맞췄어?")
-            user_said = input("답변 : ")
+            user_said = speech_to_text()
             break
 
         behavior_list.do_praise_S()
@@ -157,7 +157,7 @@ def Play_Sound(user_name):
         while True:
             text_to_speech("준비가 됐으면 준비됐어 라고 말해줘~")
 
-            user_said = input("답변 : ")
+            user_said = speech_to_text()
             answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
             if answer == 'DONE':
@@ -176,7 +176,7 @@ def Play_Sound(user_name):
         behavior_list.do_question_S()
         while True:
             text_to_speech("어떤 소리가 나? 정답을 맞췄어?")
-            user_said = input("답변 : ")
+            user_said = speech_to_text()
             break
 
         behavior_list.do_praise_S()
@@ -192,7 +192,7 @@ def Play_Sound(user_name):
     while True:
         text_to_speech("한 번 더 해볼까? 또 하고 싶으면 또 하자라고 말해줘.")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
         if answer == 'AGAIN':
@@ -218,7 +218,7 @@ def Play_Sound(user_name):
 
         text_to_speech("정리가 끝나면 다 했어 라고 말해줘.")
         
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
         if answer == 'DONE':
@@ -238,11 +238,11 @@ def Play_Sound(user_name):
         time.sleep(1)
         text_to_speech("오늘 소리 맞추기 놀이할 때, 문제 내는게 재미있었어, 맞추는게 재미있었어?")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
 
         text_to_speech("정말? 어떤 소리가 가장 맞추기 어려웠어?")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         break
 
     behavior_list.do_praise_L()
@@ -272,7 +272,7 @@ def Play_Sound(user_name):
         time.sleep(1)
         text_to_speech("또 다른 놀이 할까? 파이보랑 또 놀고 싶으면 놀고 싶다고 말해줘!")
 
-        user_said = input("답변 : ")
+        user_said = speech_to_text()
         answer = NLP.nlp_answer(user_said=user_said, dic=Dic)
 
         if answer == 'AGAIN':       # 지금은 어떤 답변이라도 프로그램 종료됨
